4e5218d093f37cd148b3d97e48f9ed15d98f6450
summer <summer@summers-MacBook-Pro-4.local>
2019-07-26 13:41:48 -0700

    my first github repo

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-//ā8b0č3d0ę7f0g0h0ì6j0k0ł1m0ńõ8p0q0r0š3t0ū5v0wx0ÿ1ż3
-alert("bruh");
-alert("moment");
+var yourChoice = "";
+
+var userHP = 100;
+var userMP = 80;
+var userDMG = 10;
+var userSpecial = 30;
+
+var scorecount = 0;
+
+$("#choosechar").click(play);
+
+function play() {
+
+yourChoice = $("#dropdown").val();
+
+if(yourChoice == "warrior"){//selected warrior
+    userHP = 100;
+    userMP = 80;
+    userDMG = 10;
+    userSpecial = 30;
+}
+if(yourChoice == "knight"){//selected knight
+    userHP = 150;
+    userMP = 100;
+    userDMG = 5
+    userSpecial = 20;
+}
+if(yourChoice == "berserker"){//selected berserker
+    userHP = 50;
+    userMP = 60;
+    userDMG = 20;
+    userSpecial = 50;
+}
+console.log("health = " + userHP);
+console.log("MP = " + userMP);
+console.log("damage = " + userDMG);
+console.log("special dmg = " + userSpecial);
+}
+console.log(yourChoice);
